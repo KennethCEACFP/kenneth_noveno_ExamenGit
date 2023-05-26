7bb25c40ba486ebf706ab8b1592a83b365f372cd
JohnNoveno-AlumnoCEA <JohnNoveno-AlumnoCEA@LTP1532022>
2023-05-26 12:19:12 +0200

    Agregar clase.java en la primera rama

---
+++ examenGit/src/examenGit/Clase.java
@@ -0,0 +1,14 @@
+package examenGit;
+
+public class Clase {
+
+	public static void main(String[] args) {
+		String nombre = "Kenneth";
+		int edad= 19;
+		
+		System.out.println(nombre);
+		System.out.println(edad);
+
+	}
+
+}
