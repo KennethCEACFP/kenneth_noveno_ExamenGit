49f973f9e5a8cc4888f620ccaee13556eff92071
JohnNoveno-AlumnoCEA <JohnNoveno-AlumnoCEA@LTP1532022>
2023-05-26 12:32:19 +0200

    Creacón de un nuevo fichero en el paquete 2 en la rama2

---
+++ examenGit/src/paquete2/clase2.java
@@ -0,0 +1,13 @@
+package paquete2;
+
+public class clase2 {
+
+	public static void main(String[] args) {
+		int num1= 2;
+		int num2= 3;
+		int resultado = num1+num2;
+		System.out.println(resultado);
+
+	}
+
+}
