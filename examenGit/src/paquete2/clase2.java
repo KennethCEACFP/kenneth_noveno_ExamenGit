5c06639ad32c652ef8ad33e33377f213fc2418e9
JohnNoveno-AlumnoCEA <JohnNoveno-AlumnoCEA@LTP1532022>
2023-05-26 12:45:12 +0200

    cambio clase2

--- examenGit/src/paquete2/clase2.java
+++ examenGit/src/paquete2/clase2.java
@@ -3,8 +3,8 @@ package paquete2;
 public class clase2 {
 
 	public static void main(String[] args) {
-		int num1= 2;
-		int num2= 3;
+		int num1= 7;
+		int num2= 2;
 		int resultado = num1+num2;
 		System.out.println(resultado);
 
